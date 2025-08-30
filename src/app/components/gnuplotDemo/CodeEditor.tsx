3f821d3315ee36df9e2db40bd8102c79a774ce0c
Mapaor <mpardoor24@alumnes.ub.edu>
2025-08-30 21:36:46 +0200

    simplify

--- src/app/components/gnuplotDemo/CodeEditor.tsx
+++ src/app/components/gnuplotDemo/CodeEditor.tsx
@@ -52,11 +52,11 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
             <div className="text-xs text-gray-500">
               Nota: El &quot;set terminal svg&quot; i &quot;set output &apos;plot.svg&apos;&quot; s&apos;han de posar sempre.
             </div>
-            {selectedExample?.data && (
+            {/* {selectedExample?.data && (
               <div className="text-xs text-green-600 italic">
                 * Aquest exemple utilitza dades externes. Pots veure-les a la pestanya &quot;Data&quot;.
               </div>
-            )}
+            )} */}
           </div>
         </>
       ) : (
@@ -82,13 +82,13 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
           />
           <div className="mt-3 flex flex-col gap-1">
             <div className="text-xs text-gray-500">
-              Nota: Aquestes dades es guarden en un fitxer anomenat &quot;data.dat&quot; que pots utilitzar en el codi gnuplot amb <code className="bg-gray-100 px-1 rounded">plot &quot;data.dat&quot; using 1:2</code>
+              Nota: Aquestes dades es guarden en un fitxer anomenat &quot;data.dat&quot; que pots utilitzar en el codi gnuplot amb <code className="bg-gray-100 px-1 rounded">plot &quot;data.dat&quot;</code>.
             </div>
-            {selectedExample?.data && (
+            {/* {selectedExample?.data && (
               <div className="text-xs text-green-600 italic">
                 * Les dades actuals provenen de l&apos;exemple &quot;{selectedExample.title}&quot;
               </div>
-            )}
+            )} */}
           </div>
         </>
       )}
